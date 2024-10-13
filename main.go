0d5791c1a2385fb092f6fe91863bf603a4d3e680
Zack-Ali-Hassan <yares6667@gmail.com>
2024-10-13 21:45:25 +0300

    third commit in 13-oct-2024

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	// "github.com/gofiber/fiber/v2/middleware/cors"
@@ -20,7 +19,6 @@ type Blog struct {
 	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Title   string             `json:"title"`
 	Content string             `json:"content"`
-	Date    time.Time          `json:"date"`
 }
 
 var collection *mongo.Collection
